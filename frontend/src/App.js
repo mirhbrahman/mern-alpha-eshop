ace1055416cd46cbf215ddebce80f5385b16e26e
WHOAMI <alphawhoami@gmail.com>
2020-12-21 00:02:22 +0600

    Header setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = ()=> {
-  return (
-    <>
-      <p>Welcome to proshop</p>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <p>Welcome to proshop</p>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
