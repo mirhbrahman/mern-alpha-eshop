264b22f5dbe87cf0ada0aa85a1cd2a31b842ed96
WHOAMI <alphawhoami@gmail.com>
2021-01-17 23:19:41 +0600

    Added product route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import donenv from 'dotenv'
+import productRoute from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js"
-import products from './data/products.js'
 
 donenv.config()
 
@@ -13,14 +14,11 @@ app.get('/', (req, res)=>{
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRoute)
 
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(p => p._id == req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
