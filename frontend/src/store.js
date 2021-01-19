46c86b13f5f476107b0e4c17f24da496e319697b
WHOAMI <alphawhoami@gmail.com>
2021-01-19 22:14:30 +0600

    Added product details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer } from "./reducers/productReducers";
+import {productDetailsReducer} from "./reducers/productDetailsReducers";
 
 
 const reducer = combineReducers({
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailsReducer
 })
 const initialState = {}
 const middleware = [thunk]
