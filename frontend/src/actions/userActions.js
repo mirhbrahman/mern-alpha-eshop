e57892345854e151367ad733f0d84f396fe6a0da
WHOAMI <alphawhoami@gmail.com>
2021-02-06 20:36:53 +0600

    Added profile view and profile update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,9 +1,17 @@
 import {
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
-    USER_LOGOUT, USER_REGISTER_FAIL,
-    USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS
+    USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -72,6 +80,67 @@ export const register = (name, email, password) => async (dispatch)=>{
     }catch (e) {
         dispatch({
             type: USER_REGISTER_FAIL,
+            payload: e.response && e.response.data.message ? e.response.data.message : e.message
+        })
+    }
+}
+
+
+export const getUserDetails = () => async (dispatch, getState)=>{
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST
+        })
+
+        const { userLogin: {userInfo} } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get('/api/users/profile', config)
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    }catch (e) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: e.response && e.response.data.message ? e.response.data.message : e.message
+        })
+    }
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState)=>{
+    try {
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const { userLogin: {userInfo} } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put('/api/users/profile', user, config)
+
+        dispatch({
+            type: USER_UPDATE_PROFILE_SUCCESS,
+            payload: data
+        })
+
+    }catch (e) {
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload: e.response && e.response.data.message ? e.response.data.message : e.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,12 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer } from "./reducers/productReducers";
 import { productDetailsReducer } from "./reducers/productDetailsReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import {
+    userDetailsReducer,
+    userLoginReducer,
+    userRegisterReducer,
+    userUpdateProfileReducer
+} from "./reducers/userReducers";
 
 
 const reducer = combineReducers({
@@ -12,7 +17,9 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart : cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
