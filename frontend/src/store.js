a70f32e34be3a36e19cb1f527c27cf277b0ad622
WHOAMI <alphawhoami@gmail.com>
2021-02-06 19:34:00 +0600

    Added user registration

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,14 +4,15 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer } from "./reducers/productReducers";
 import { productDetailsReducer } from "./reducers/productDetailsReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart : cartReducer,
-    userLogin: userLoginReducer
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
