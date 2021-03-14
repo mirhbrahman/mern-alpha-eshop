42e0230ae8dbab976e5ad11116dfb50d490203af
WHOAMI <alphawhoami@gmail.com>
2021-03-15 00:01:35 +0600

    Added product create and update api endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,22 @@
 import express from 'express'
 const router = express.Router()
-import {deleteProduct, getProductById, getProducts} from "../controllers/productController.js";
+import {
+    createProduct,
+    deleteProduct,
+    getProductById,
+    getProducts,
+    updateProduct
+} from "../controllers/productController.js";
 import {admin, protect} from "../middleware/authMiddleware.js";
 
 
-router.route('/').get(getProducts)
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
 router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 
 export default router
