bb6831ef38db514561174115137ce683e71e6dea
WHOAMI <alphawhoami@gmail.com>
2021-03-27 20:12:24 +0600

    Ready for production

--- backend/server.js
+++ backend/server.js
@@ -47,5 +47,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
